0df93ec3f455b5f6e7ffc444c1ec6c327a4c6f0f
sufyanali <sufi404040@gmail.com>
2025-01-20 22:35:26 +0500

    change in multiple file

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -4,7 +4,13 @@
 **/
 import { defineCliConfig } from 'sanity/cli'
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+const projectId = 'c1xnfnnm'
+const dataset = 'production'
 
-export default defineCliConfig({ api: { projectId, dataset },studioHost: 'my-comforty-chair' })
+export default defineCliConfig({ 
+    api: { 
+        projectId, 
+        dataset 
+    },
+    studioHost: 'my-comforty-chair',
+})
